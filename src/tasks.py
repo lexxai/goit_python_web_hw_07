30886dd8f06d289c4285b2c000980c983fae2287
lexxai <lexxai@users.noreply.github.com>
2023-10-02 22:01:20 +0300

    task_01

--- src/tasks.py
+++ src/tasks.py
@@ -1,18 +1,40 @@
-from sqlalchemy import func, select
+from sqlalchemy import func, select, label, desc
 
 from database.db import session
 from database.models import Student, Teacher, Group, Grade, Discipline
 
 
-def task_01():
+def get_query_dict(query):
+    return {"column_names": query.statement.columns.keys(), "result": query.all()}
+
+
+def task_01(*args, **kwargs):
+    """
+    SELECT s.fullname as student, ROUND(AVG(grade),2) as average_grade
+    FROM grade g
+    LEFT JOIN students s ON s.id = g.students_id
+    GROUP BY s.id
+    ORDER BY average_grade DESC
+    LIMIT 5
+    """
+    query = (
+        session.query(
+            func.CONCAT(Student.first_name, " ", Student.last_name).label("Student"),
+            func.ROUND(func.AVG(Grade.grade), 2).label("average_grade"),
+        )
+        .select_from(Grade)
+        .join(Student)
+        .group_by(Student.id)
+        .limit(5)
+    )
+    return get_query_dict(query)
+
+
+def task_02(*args, **kwargs):
     r = session.query()
 
 
-def task_02():
-    r = session.query()
-
-
-def get_tasks(obj = None):
+def get_tasks(obj=None):
     if obj is None:
         obj = globals()
     tasks = [
@@ -31,10 +53,14 @@ if __name__ == "__main__":
     # func = globals().get(tasks[0])
     # func()
     for task in get_tasks():
-        print("-"*80)
+        print("-" * 80)
         print(task.__name__)
-        result = task()
-        print(f"result = {result}")
-        
-
-
+        task_result = task()
+        if task_result:
+            column_names = task_result.get("column_names")
+            for row in enumerate(task_result.get("result")):
+                row_str = []
+                for i, col in enumerate(row[1]):
+                    row_str.append(f"{column_names[i]}: {col}")
+                result = ", ".join(row_str)
+                print(result)
